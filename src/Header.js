9403c2144a318f1a83872fa4d7f05fcc37bd66f0
Rushui Fang <rfang4@binghamton.edu>
2020-08-25 20:36:49 -0400

    add login

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,17 @@ import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 
 function Header() {
-    const [{ basket }] = useStateValue();
+    const [{ basket, user }] = useStateValue();
+
+    const login = () => {
+        if (user) {
+            auth.signOut();
+        }
+    };
+
     console.log(basket);
     return (
         <nav className="header">
@@ -22,10 +30,14 @@ function Header() {
                 <SearchIcon className="header_searchIcon" />
             </div>
             <div className="header_nav">
-                <Link to="/login" className="header_link">
-                    <div className="header_option">
-                        <span className="header_optionLineOne">Hello Qazi</span>
-                        <span className="header_optionLineTwo">Sign In</span>
+                <Link to={!user && "/login"} className="header_link">
+                    <div onClick={login} className="header_option">
+                        <span className="header_optionLineOne">
+                            Hello {user?.email}
+                        </span>
+                        <span className="header_optionLineTwo">
+                            {user ? "Sign Out" : "Sign In"}
+                        </span>
                     </div>
                 </Link>
                 <Link to="/" className="header_link">
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyCLiSNffcsNre7ZAYN6nC_RiqCUbjI26y0",
+    authDomain: "clone-61136.firebaseapp.com",
+    databaseURL: "https://clone-61136.firebaseio.com",
+    projectId: "clone-61136",
+    storageBucket: "clone-61136.appspot.com",
+    messagingSenderId: "683525286285",
+    appId: "1:683525286285:web:fb37081d8afa9a8206b5c4",
+    measurementId: "G-JSHC1M99CR",
+});
+
+const auth = firebase.auth();
+export { auth };
